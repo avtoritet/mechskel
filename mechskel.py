b1f3c353037435487e89c5ae47cbd8b6498d20e8
avtoritet <alex3ller@gmail.com>
2015-08-29 22:50:06 -0400

    mechanize skeleton
    Use this mechanize skeleton to write a mechanize program, adding the
    needed features

---
+++ mechskel.py
@@ -0,0 +1,40 @@
+# mechanize skeleton set-up 
+# Start with this code, then add the neede functionality
+import mechanize
+import cookielib
+import webbrowser
+
+# Browser
+br = mechanize.Browser()
+
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# User-Agent -> default one honors robots.txt
+USER_AGENT = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
+
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+# Want debugging messages?
+#br.set_debug_http(True)
+#br.set_debug_redirects(True)
+#br.set_debug_responses(True)
+
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', USER_AGENT)]
+
+# Open some site
+r = br.open('http://google.com')
+html = r.read()
+
+# Show the source
+print html
